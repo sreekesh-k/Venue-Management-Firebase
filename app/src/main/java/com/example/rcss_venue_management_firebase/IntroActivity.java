7976bd69f49200526e1d950cf037cd22469d5707
Sreekesh K Prabhu <145774649+sreekesh-k@users.noreply.github.com>
2025-03-09 14:45:44 +0530

    ui fix

--- app/src/main/java/com/example/rcss_venue_management_firebase/IntroActivity.java
+++ app/src/main/java/com/example/rcss_venue_management_firebase/IntroActivity.java
@@ -1,24 +1,38 @@
 package com.example.rcss_venue_management_firebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class IntroActivity extends AppCompatActivity {
-
+    private ImageView hero;
+    private LinearLayout heading;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_intro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Animation topAnim = AnimationUtils.loadAnimation(this, R.anim.top_anim);
+        Animation bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+        hero = findViewById(R.id.imageView);
+        heading = findViewById(R.id.heading);
+
+        hero.setAnimation(topAnim);
+        heading.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 4000);
+
     }
 }
